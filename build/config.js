07c86df068d6e5e623618f5e00d57838cc6c1bd7
glt <gltwork@163.com>
2020-04-29 20:34:44 +0800

    '修改配置'

--- build/config.js
+++ build/config.js
@@ -5,10 +5,10 @@ var Components = require('../components.json');
 var externals = {};
 
 Object.keys(Components).forEach(function(key) {
-  externals[`g-ui/packages/${key}`] = `g-ui/lib/${key}`;
+  externals[`lgsn-ui/packages/${key}`] = `lgsn-ui/lib/${key}`;
 });
 
-externals['g-ui/src/locale'] = 'g-ui/lib/locale';
+externals['lgsn-ui/src/locale'] = 'lgsn-ui/lib/locale';
 
 externals = [Object.assign({
   vue: 'vue'
@@ -20,7 +20,7 @@ exports.alias = {
   main: path.resolve(__dirname, '../src'),
   packages: path.resolve(__dirname, '../packages'),
   examples: path.resolve(__dirname, '../examples'),
-  'g-ui': path.resolve(__dirname, '../')
+  'lgsn-ui': path.resolve(__dirname, '../')
 };
 
 exports.vue = {
--- examples/index.tpl
+++ examples/index.tpl
@@ -3,8 +3,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <title>G-UI</title>
-    <meta name="description" content="G-UI" />
+    <title>lgsn-ui</title>
+    <meta name="description" content="lgsn-ui" />
   </head>
   <body>
     <div id="app"></div>
